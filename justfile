437d1f1ce370432449f444f8d1fbdc636360d21f
terade <134976752+terade@users.noreply.github.com.>
2023-10-15 22:54:58 +0200

    add justfile for the home directory

--- justfile
+++ justfile
@@ -1,8 +1,13 @@
 
+set shell := ["fish", "-c"]
+
 get_changes:
     cp ~/.xmonad/xmonad.hs xmonad
     cp -r ~/.config/nvim/lua nvim/
     cp -r ~/.config/nvim/after nvim/
     cp -r ~/.config/nvim/init.lua nvim/
+    cp -r ~/.config/tmux/tmux.conf .
+    cp ~/justfile ./home_justfile
 
-
+edit:
+    vim justfile
--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "vimdoc", "c", "lua", "rust", "perl" },
+  ensure_installed = { "vimdoc", "c", "lua", "rust", "perl", "go" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
